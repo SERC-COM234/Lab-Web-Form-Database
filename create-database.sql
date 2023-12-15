a65ef905c7a58cd080dda282bd5d24b2fbc1a8b7
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2023-12-15 09:30:23 +0000

    add exercise files

---
+++ create-database.sql
@@ -0,0 +1,9 @@
+/**************************************************************
+This file should contain the SQL queries to:
+- create a database called WebForm
+- create a table called Submission with the following columns:
+    - id (int, primary key, auto increment)
+    - name (varchar(50), not null)
+    - email (varchar(50), not null)
+    - message (varchar(max), not null)
+**************************************************************/
